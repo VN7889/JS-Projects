62a902bb83632bbeac24962d9095e6e965af721d
Varun N <103560317+VN7889@users.noreply.github.com>
2023-05-26 20:12:37 -0500

    Update ceaser-cipher.js

--- ceaser-cipher.js
+++ ceaser-cipher.js
@@ -4,7 +4,8 @@
 
 // Write a function which takes a ROT13 encoded string as input and returns a decoded string.
 
-All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+//All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
 function rot13(str) {
     let alph=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     let b='';
