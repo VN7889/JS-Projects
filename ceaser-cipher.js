5eddc6f915f7aee33cac4bf54dda38efb14cd894
Varun N <103560317+VN7889@users.noreply.github.com>
2023-05-26 20:08:48 -0500

    Update ceaser-cipher.js

--- ceaser-cipher.js
+++ ceaser-cipher.js
@@ -1,3 +1,10 @@
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 function rot13(str) {
     let alph=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     let b='';
@@ -13,5 +20,4 @@ function rot13(str) {
     
     return b;
   }
-  
-  console.log(rot13("SERR PBQR PNZC"));
+ 
